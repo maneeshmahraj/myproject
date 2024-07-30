4c604130f6c2056238c76e50be493317727413bd
maneeshmahraj <mb124969@gmail.com>
2024-07-30 01:08:05 -0600

    backend server create

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const app=express();
+const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
+const cors=require("cors");
+require("dotenv").config();
+const port=process.env.PORT || 7000;
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DATABASE_NAME}`)
+
+app.use(cors());
+
+
+
+app.listen(port,()=>{
+    console.log("server is running 8000 port !!")
+})
