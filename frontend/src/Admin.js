b91d2263d037ec4c7c005cc88cc4784129216476
maneeshmahraj <mb124969@gmail.com>
2024-08-01 06:21:52 -0600

    admin sidewar

--- frontend/src/Admin.js
+++ frontend/src/Admin.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Admin = () => {
   return (
     <>
-    <div className='admin-page'>Admin</div>
+    <div className='admin-page'>
+      <div className='admin-logo1'>
+        <div className='admin-log'>
+         <img src='images/mk.jpg' />
+        </div>
+        <div className='admin-log1'>
+          <p>User Name: <span>maneesh budholiya</span></p>
+          <p>mobile:<span>9140688969</span></p>
+          <button>Edit profile</button>
+        </div>
+      </div>
+      <div className='admin-logo2'>
+        <div className='admin-sidewar'>
+          <ul>
+            <li><Link to="insert" className='link'>Data Insert</Link></li>
+            <li><Link to="insert" className='link'>Data Display</Link></li>
+            <li><Link to="insert" className='link'>Data Update</Link></li>
+            <li><Link to="insert" className='link'>Ordered Items</Link></li>
+
+          </ul>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
