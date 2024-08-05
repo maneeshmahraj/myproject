5e4decf1f5b131ee1972b58965905d652447b622
maneeshmahraj <mb124969@gmail.com>
2024-08-05 12:02:18 -0600

    cart meenu

--- frontend/src/Cart.js
+++ frontend/src/Cart.js
@@ -1,11 +1,37 @@
 
-
+import { MdCurrencyRupee } from "react-icons/md";
 import React from 'react'
-
+import { BiRupee } from "react-icons/bi";
 const Cart = () => {
   return (
     <>
-    <div className='cart'>Cart</div>
+    <div className='cart'>
+       
+        <div className='cart-items-dt'>
+        <h3>Order summary</h3>
+        <div className='cart-meenu'>
+          <div className='cart-meenu1'>
+          <img src='image1/img1.jpg'/>
+          </div>
+         <p>Infinite Double Bed with Mattress, Storage and Bookshelves</p>
+         <div className='cart-meenu2'>
+      
+          <h3><MdCurrencyRupee />899/Month</h3>
+        </div>
+       <div className="cart-remove"><button>X</button></div>
+        </div>
+        </div>
+        <div className='cart-items-dt1'>
+         <h3>Payment Summary</h3>
+         <div className='item-sumry'><span>Monthly Rental</span><span><BiRupee />899/-</span></div>
+         <div className='item-sumry'><span>Delivery</span><span style={{color:"green"}}>FREE</span></div>
+         <div className='item-sumry'><span>Installation</span><span style={{color:"green"}}>FREE</span></div>
+         <div className='item-sumry'><span style={{color:" rgb(26, 5, 5)",fontWeight:"bold"}}>Total Payable : </span><span style={{color:" rgb(26, 5, 5)",fontWeight:"bold"}}><BiRupee />1800/-</span></div>
+         <button className='payment'>Payment</button>
+           
+        </div>
+       
+    </div>
     </>
   )
 }
--- frontend/src/Content.js
+++ frontend/src/Content.js
@@ -10,29 +10,29 @@ const Content = () => {
     <div className='content'>
       <div className='heading-promiss'><h1>WE PROMISE</h1></div>
       <div className='icon-promiss'>
-        <div className="icon-promiss-item"  style={{borderRight:"1px solid gray"}}>
+        <div className="icon-promiss-item"  >
         <img src='images/p1.jpeg' alt='p' className="icon-promiss-item1" style={{width:"70px",height:"70px"}}/>
        
         <span className='setting-word'>Well-Crafted <br/>Packages</span>
         </div>
-        <div className='icon-promiss-item' style={{borderRight:"1px solid gray"}}>
+        <div className='icon-promiss-item'>
         <img src='images/p2.jpeg' alt='p' className="icon-promiss-item2" style={{width:"70px",height:"70px"}}/>
         <span className='setting-word'>Free Delivery & <br/>installation</span>
 
         </div>
-        <div className='icon-promiss-item' style={{borderRight:"1px solid gray",flexDirection:"column"}}>
+        <div className='icon-promiss-item' style={{flexDirection:"column"}}>
         <img src='images/p5.jpeg' alt='p' className="icon-promiss-item3" style={{width:"100px",height:"100px"}}/>
 
         <p className='setting-word'>Free Yearly <br/>Maintainance</p>
       
         </div>
-        <div className='icon-promiss-item' style={{borderRight:"1px solid gray"}}>
+        <div className='icon-promiss-item' >
         <img src='images/p6.jpeg' alt='p' className="icon-promiss-item4" style={{width:"70px",height:"70px"}}/>
 
         <p className='setting-word'>Free Yearly <br/>Maintainance</p>
 
         </div>
-        <div className='icon-promiss-item'style={{borderRight:"1px solid gray"}}>
+        <div className='icon-promiss-item'>
         <img src='images/p3.png' alt='p' className="icon-promiss-item5" style={{width:"70px",height:"70px"}}/>
 
 
